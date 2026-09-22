4e9f37aba1961d6eee16b602d5233bcaecbb99ac
Shrimpsnail <116761665+Shrimpsnail@users.noreply.github.com>
2026-09-22 21:28:00 +0100

    Actual cleanup and new consumables commit

---
+++ Shrimple.lua
@@ -0,0 +1,9 @@
+
+sendDebugMessage("oh this is not gonna look good with mods")
+
+
+SMODS.Atlas{key = "cards_1", path = "8BitDeck.png", px = 74, py = 98, prefix_config = { key = false } }
+SMODS.Atlas{key = "centers", path = "Enhancers.png", px = 74, py = 98, prefix_config = { key = false } }
+SMODS.Atlas{key = "Tarot", path = "Tarots.png", px = 74, py = 98, prefix_config = { key = false } }
+SMODS.Atlas{key = "Spectral", path = "Tarots.png", px = 74, py = 98, prefix_config = { key = false } }
+SMODS.Atlas{key = "Planet", path = "Tarots.png", px = 74, py = 98, prefix_config = { key = false } }
